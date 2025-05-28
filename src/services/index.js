0bfb59778cb67d5d45c2a07c11ff5c20fad1f8e5
vitoriaAlbuquerque07 <vitoriam.albuquerque00@gmail.com>
2025-05-28 17:14:29 -0300

    API

---
+++ src/services/index.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql2/promise");
+
+async function executarSQL(comandoSql){
+    const conexao = await mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "1234",
+        port: "3306",
+        database: "dripstore"
+    });
+
+    const [result] = await conexao.query(comandoSql);
+    conexao.end();
+
+    return result;
+}
+
+module.exports = { executarSQL };
